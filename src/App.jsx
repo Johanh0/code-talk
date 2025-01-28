3f82be250a01869c040399c02dec7b2f037afa7f
Johan Herrera <johanherrera20000@gmail.com>
2025-01-27 19:26:57 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Import Components
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main></Main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoPlayer = () => {
+  return (
+    <section>
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default VideoPlayer;
